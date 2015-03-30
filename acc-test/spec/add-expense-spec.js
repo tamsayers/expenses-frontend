5137155a6a29467b8c0ba73c95c21397ba892d05
Tam Sayers <tamsayers@hotmail.com>
2015-03-30 22:15:39 +0100

    partially implemented protractor test

--- acc-test/spec/add-expense-spec.js
+++ acc-test/spec/add-expense-spec.js
@@ -2,11 +2,11 @@ describe('angularjs homepage todo list', function() {
   it('should add a todo', function() {
     browser.get('http://localhost:8080/index.html');
 
-    element(by.model('expense.description')).sendKeys('write a protractor test');
-//    element(by.css('[value="add"]')).click();
-//
-//    var todoList = element.all(by.repeater('todo in todos'));
-//    expect(todoList.count()).toEqual(3);
-//    expect(todoList.get(2).getText()).toEqual('write a protractor test');
+    element(by.model('expense.date')).sendKeys('2015-01-01');
+    element(by.model('expense.description')).sendKeys('Description 1');
+    element(by.model('expense.clientName')).sendKeys('Client');
+    element(by.model('expense.supplier')).sendKeys('Supplier');
+    element(by.model('expense.cost.amount')).sendKeys('1.99');
+    element(by.model('expense.cost.costType')).element(by.css('option[value="Simple"]')).click();
   });
 });
