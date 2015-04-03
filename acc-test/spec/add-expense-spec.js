42205747180bcd8e2650e3342b1ae9b595f04c90
Tam Sayers <tamsayers@hotmail.com>
2015-04-03 20:39:52 +0100

    added page object for protractor test

--- acc-test/spec/add-expense-spec.js
+++ acc-test/spec/add-expense-spec.js
@@ -1,12 +1,37 @@
+var AddExpensesPage = function() {
+  var page = this;
+  page.go = function() {
+    browser.get('http://localhost:8080/index.html');
+  };
+  page.date = element(by.model('expense.date'));
+  page.description = element(by.model('expense.description'));
+  page.clientName = element(by.model('expense.clientName'));
+  page.supplier = element(by.model('expense.supplier'));
+  page.cost = {
+      amount: element(by.model('expense.cost.amount')),
+      type: element(by.model('expense.cost.costType'))
+  };
+  page.submitButton = element(by.css('.expenses-form input[type=submit]'));
+  
+  page.cost.type.select = function(value) {
+    page.cost.type.element(by.css('option[value="Simple"]')).click();
+  };
+}
+
 describe('angularjs homepage todo list', function() {
   it('should add a todo', function() {
-    browser.get('http://localhost:8080/index.html');
+    var addExpensesPage = new AddExpensesPage();
 
-    element(by.model('expense.date')).sendKeys('2015-01-01');
-    element(by.model('expense.description')).sendKeys('Description 1');
-    element(by.model('expense.clientName')).sendKeys('Client');
-    element(by.model('expense.supplier')).sendKeys('Supplier');
-    element(by.model('expense.cost.amount')).sendKeys('1.99');
-    element(by.model('expense.cost.costType')).element(by.css('option[value="Simple"]')).click();
+    addExpensesPage.go();
+    addExpensesPage.date.sendKeys('2015-01-01');
+    addExpensesPage.description.sendKeys('Description 1');
+    addExpensesPage.clientName.sendKeys('Client');
+    addExpensesPage.supplier.sendKeys('Supplier');
+    addExpensesPage.cost.amount.sendKeys('1.99');
+    addExpensesPage.cost.type.select("Simple");
+    
+    addExpensesPage.submitButton.click();
+    
+    // verify saved - move page to expenses list?
   });
 });
