e7ef667038887648c5f779194e24f4b12670010d
Tam Sayers <tamsayers@hotmail.com>
2015-04-06 21:28:30 +0100

    added add expense function and test

--- src/expenses-form.js
+++ src/expenses-form.js
@@ -1,6 +1,11 @@
 // service for api requests - put in separate file?
 expensesApp.controller('ExpensesFormController', ['$scope', 'apiRequest', function($scope, apiRequest) {
   $scope.postExpenses = function() {
-    apiRequest.post('/expenses', [$scope.expense]);
+    apiRequest.post('/expenses', $scope.expenses);
+  };
+  $scope.expenses = [{}];
+  
+  $scope.addExpense = function() {
+      $scope.expenses.push({});
   };
 }]);
--- test/expenses-form-spec.js
+++ test/expenses-form-spec.js
@@ -1,25 +1,43 @@
 describe('expenses controller function', function() {
-
-  describe('form submit', function() {
-    var $scope, apiRequest;
-
     beforeEach(module('expensesApp'));
 
+    var $scope, apiRequest;
+
     beforeEach(inject(function($rootScope, $controller) {
-      $scope = $rootScope.$new();
-      $scope.expense = {
-          description: "desc"
-      }
-      apiRequest = {
-          post: function() {}
-      };
-      spyOn(apiRequest, 'post');
-      $controller('ExpensesFormController', {$scope: $scope, apiRequest: apiRequest});
+        $scope = $rootScope.$new();
+
+        apiRequest = {
+            post : function() {}
+        };
+        $controller('ExpensesFormController', {
+            $scope : $scope,
+            apiRequest : apiRequest
+        });
     }));
 
-    it('should use api to post expenses data', function() {
-      $scope.postExpenses();
-      expect(apiRequest.post).toHaveBeenCalledWith('/expenses', [$scope.expense]);
+    describe('form submit', function() {
+        beforeEach(function() {
+            $scope.expenses = [ {
+                description : "desc1"
+            }, {
+                description : "desc2"
+            }];
+            
+            spyOn(apiRequest, 'post');
+        });
+
+        it('should use api to post expenses data', function() {
+            $scope.postExpenses();
+            expect(apiRequest.post).toHaveBeenCalledWith('/expenses', $scope.expenses);
+        });
     });
-  });
+
+    describe('add expense', function() {
+        it('should add another expense object to the scope', function() {
+            expect($scope.expenses.length).toBe(1);
+            $scope.addExpense();
+            $scope.addExpense();
+            expect($scope.expenses.length).toBe(3);
+        });
+    });
 });
